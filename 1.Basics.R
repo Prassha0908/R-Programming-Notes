7f2d53f800977b6f8f36c611aef89e3634b6d852
Prasshanthini R <107397262+Prassha0908@users.noreply.github.com>
2025-05-09 15:32:22 +0530

    Basics of R program

---
+++ 1.Basics.R
@@ -0,0 +1,39 @@
+# Basic R Program - refer READme file in-case of any doubt.
+
+## Assigning Variables 
+
+X <- 3
+Y <- 5
+
+## Arithmetic Operations
+
+X+Y
+X-Y
+X*Y
+X/Y
+a <- X^Y
+a 
+
+## Printing Values
+
+print(X-Y)  #  [1]  -2
+cat("Sum :",X+Y,"\n")    # Sum : 8
+
+## Valid assigning 
+
+value <- 10
+1value <- 10 # Invalid: 1value <- 10  (starts with number)
+
+## Alternative assignment
+
+20 -> z
+print(z)
+
+## Check existing variables
+
+ls()
+
+## Remove a variable
+
+rm(z)
+z #Error: object 'z' not found
